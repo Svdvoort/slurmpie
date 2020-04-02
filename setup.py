1278076e4aed21ad0dad693cd2d302ef4dfd9570
Sebastian <Svdvoort@users.noreply.github.com>
2020-04-02 17:38:20 +0200

    Fix in package building workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="slurmpie",
-    version="0.1.0",
+    version="0.1.5",
     author="Sebastian van der Voort",
     author_email="svoort25@gmail.com",
     description="Package to interact with SLURM",
