67141c6c692c68f9834906d9844096d8b671c97c
svdvoort <23049683+Svdvoort@users.noreply.github.com>
2024-04-03 15:46:17 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="slurmpie",
-    version="0.8.0",
+    version="0.8.1",
     author="Sebastian van der Voort",
     author_email="Svdvoort@users.noreply.github.com",
     description="Package to interact with SLURM",
